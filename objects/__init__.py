f32a5e5dd2c4e02a9716cfe730af549068131872
Iuryglabson <45886156+iurysilva@users.noreply.github.com>
2020-06-16 23:39:57 -0300

    Nothing

--- objects/__init__.py
+++
@@ -1,3 +0,0 @@
-from objects.chromossome import Chromossome
-from objects.genetic_algorithm import GeneticAlgorithm
-from objects.population import Population
--- procedures/__init__.py
+++
@@ -1,5 +0,0 @@
-from procedures.crossover_methods import *
-from procedures.do_one_generation import do_one_generation
-from procedures.make_histogram import make_histogram
-from procedures.update_angle_list import update_angle_list
-
