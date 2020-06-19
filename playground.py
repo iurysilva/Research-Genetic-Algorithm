868a8f82b8f284ead73e923d2ab1c7e2277cfa6c
Iuryglabson <45886156+iurysilva@users.noreply.github.com>
2020-06-19 16:42:03 -0300

    Refactor: Set playground variables

--- playground.py
+++ playground.py
@@ -7,14 +7,14 @@ from run_options import run_animation_2d
 
 
 crossover_chance = 0.80
-mutation_chance = 0.20
+mutation_chance = 0.02
 standart_deviation = 3  # Will be used in the Gaussian Mutation
-iterations = 100
-function = Bukin6()  # Eggholder(), Sphere(), Bukin6() or Cross()
-chromossomes_number = 100
+iterations = 1000
+function = Sphere()  # Eggholder(), Sphere(), Bukin6() or Cross()
+chromossomes_number = 50
 crossover_method = arithmetic  # arithmetic
-animation_velocity = 2000  # In millisecond's
-animation = True
+animation_velocity = 20  # In millisecond's
+animation = False
 
 # creating project
 genetic_algorithm = GeneticAlgorithm(iterations, chromossomes_number, standart_deviation, mutation_chance, function,
