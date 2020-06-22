719598f3e5f1a3b1b9621fbb46b3baea60ffb8c7
Iuryglabson <45886156+iurysilva@users.noreply.github.com>
2020-06-22 15:55:30 -0300

    Refactor: Change add_angles

--- procedures/update_angle_methods.py
+++ procedures/update_angle_methods.py
@@ -14,13 +14,10 @@ def fix_angles(angles):
     return angles
 
 
-def add_angles(new_angles, r1, r2, kids, i):
-    if angle_is_valid(r1, kids[i]):
-        angle1 = np.arctan2(r1[1], r1[0]) * 180 / np.pi
-        new_angles = np.append(new_angles, angle1)
-    if angle_is_valid(r2, kids[i + 1]):
-        angle2 = np.arctan2(r2[1], r2[0]) * 180 / np.pi
-        new_angles = np.append(new_angles, angle2)
+def add_angles(new_angles, r, kid):
+    if angle_is_valid(r, kid):
+        angle = np.arctan2(r[1], r[0]) * 180 / np.pi
+        new_angles = np.append(new_angles, angle)
     return new_angles
 
 
@@ -39,9 +36,30 @@ def arctan_with_parents_position(genetic_algorithm, population, kids):
         for i in range(0, genetic_algorithm.chromossomes_number, 2):
             r1 = kids[i].position - kids[i].dad
             r2 = kids[i+1].position - kids[i+1].mom
-            new_angles = add_angles(new_angles, r1, r2, kids, i)
+            new_angles = add_angles(new_angles, r1, kids[i])
+            new_angles = add_angles(new_angles, r2, kids[i+1])
         new_angles = fix_angles(new_angles)
         population.chromossomes_angles = np.concatenate((population.chromossomes_angles, new_angles))
 
 
-
+def arccos_with_parents_position(genetic_algorithm, population, kids):
+    new_angles = np.array([])
+    if genetic_algorithm.function.dimensions == 2:
+        for i in range(0, genetic_algorithm.chromossomes_number, 2):
+            r1 = kids[i].position - kids[i].dad
+            r2 = kids[i+1].position - kids[i+1].mom
+            if angle_is_valid(r1, kids[i]):
+                denominator = np.abs(kids[i].dad) * np.abs(r1)
+                if np.any(denominator) == 0:
+                    denominator += 0.1
+                product = np.dot(kids[i].dad, r1)
+                angle1 = np.degrees(np.arccos(product/np.abs(kids[i].dad)*np.abs(r1)))
+                new_angles = np.append(new_angles, angle1)
+            if angle_is_valid(r2, kids[i+1]):
+                denominator = np.abs(kids[i+1].mom)*np.abs(r2)
+                if np.any(denominator) == 0:
+                    denominator += 0.1
+                product = np.dot(kids[i+1].mom, r2)
+                angle2 = np.degrees(np.arccos(product/denominator))
+                new_angles = np.append(new_angles, angle2)
+        population.chromossomes_angles = np.concatenate((population.chromossomes_angles, new_angles))
